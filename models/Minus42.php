f2ed0f9cc840ec3b65e71e109d933abe057980a5
Artem Pakhomov <setnemo@gmail.com>
2019-01-19 20:09:38 +0200

    #5 Added models

---
+++ models/Minus42.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "m42".
+ *
+ * @property int $id
+ * @property string $xlogin
+ * @property int $puid
+ * @property string $updated_at
+ */
+class Minus42 extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'm42';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['puid'], 'integer'],
+            [['updated_at'], 'safe'],
+            [['xlogin'], 'string', 'max' => 25],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'xlogin' => Yii::t('app', 'Xlogin'),
+            'puid' => Yii::t('app', 'Puid'),
+            'updated_at' => Yii::t('app', 'Updated At'),
+        ];
+    }
+}
