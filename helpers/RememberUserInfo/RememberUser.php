ae86788ed31aee44ebe0fb0a4c2c835750bf4b94
vbrazas <vbrazas@student.unit.ua>
2019-04-23 00:39:07 +0300

    [#129] now RememberUser is working properly, but I still need to know where to find the kick, hours and lasthours!

--- helpers/RememberUserInfo/RememberUser.php
+++ helpers/RememberUserInfo/RememberUser.php
@@ -24,16 +24,18 @@ class RememberUser extends RememberHelper
      */
     protected function remember()
     {
+        $user = $this->ARcollection[$this->findARbyId($this->ARcollection)];
+
         $this->responseSubset['lastloc'] = date('Y-m-d H:i:s');
         $this->responseSubset['howach'] = count($this->responseSubset['achievements']);
-        $this->responseSubset['needupd'] = 0;
-        $this->responseSubset['kick'] = 0;
 
         $this->responseSubset['campus'] = $this->responseSubset['campus'][0]['name'];
         $this->responseSubset['visible'] = ($this->responseSubset['campus'] === 'Kyiv');
 
-        $this->responseSubset['hours'] = 0; // ! Need help
-        $this->responseSubset['lasthours'] = 0; // ! Need help
+        $this->responseSubset['needupd'] = 0;
+        $this->responseSubset['kick'] = $user->kick ?? 0; // ! Need help
+        $this->responseSubset['hours'] = $user->hours ?? 0; // ! Need help
+        $this->responseSubset['lasthours'] = $user->lasthours ?? 0; // ! Need help
 
         self::swapKeysInArr($this->responseSubset, [ 'id' => 'xid', 'staff?' => 'staff' ]);
         self::setTrueFalse($this->responseSubset['staff']);
