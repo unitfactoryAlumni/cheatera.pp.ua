77e91d22f73031c5ed7a9487a4136d91fa67107e
Artem Pakhomov <setnemo@gmail.com>
2019-07-10 11:10:53 +0300

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -8,7 +8,7 @@
     <div class="jumbotron">
         <h1>Welcome!</h1>
         <h4><?= Yii::t('app', 'Server paid until September 17, 2019')?></h4>
-        <p><a href="https://monobank.ua/r/ffYN" target="_blank">Donate</a></p>
+        <p><a href="https://send.monobank.com.ua/GRvvKkvg" target="_blank">Donate</a></p>
     </div>
 </div>
 
