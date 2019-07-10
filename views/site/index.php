2ea1e5580be7b5231554881913e3bf464e2d6ee1
Artem Pakhomov <setnemo@gmail.com>
2019-07-10 11:05:47 +0300

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -7,88 +7,9 @@
 
     <div class="jumbotron">
         <h1>Welcome!</h1>
-        <h4><?= Yii::t('app', 'If you have a trouble with style - please use hard refresh on page(refresh page with shift), or delete all you cookies')?></h4>
-
+        <h4><?= Yii::t('app', 'Server paid until September 17, 2019')?></h4>
+        <p><a href="https://monobank.ua/r/ffYN" target="_blank">Donate</a></p>
     </div>
-
-<!--        <div class="body-content">-->
-<!---->
-<!--        <div class="row">-->
-<!---->
-<!--            <div class="col-md-4 col-lg-4 col-sm-12">-->
-<!--                <div class="card">-->
-<!--                    <div class="card-body">-->
-<!--                        <h4 class="card-title">Exp calculator</h4>-->
-<!--                        <p class="card-text">forecasting your level</p>-->
-<!--                    </div>-->
-<!--                    <div class="card-footer">-->
-<!--                        <a href="/calculator/" class="card-link">Open calculator</a>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
-<!---->
-<!--            <div class="col-md-4 col-lg-4 col-sm-12">-->
-<!--                <div class="card">-->
-<!--                    <div class="card-body">-->
-<!--                        <h4 class="card-title">Show online friends</h4>-->
-<!--                        <p class="card-text">If your added frineds - see who is online now!</p>-->
-<!--                    </div>-->
-<!--                    <div class="card-footer">-->
-<!--                        <a href="/friends/" class="card-link">Show online friends</a>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--            <div class="col-md-4 col-lg-4 col-sm-12">-->
-<!--                <div class="card">-->
-<!--                    <div class="card-body">-->
-<!--                        <h4 class="card-title">Student table</h4>-->
-<!--                        <p class="card-text">Comparison table of students by level</p>-->
-<!--                    </div>-->
-<!--                    <div class="card-footer">-->
-<!--                        <a href="/students/" class="card-link">Show students table</a>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--            <div class="col-md-4 col-lg-4 col-sm-12">-->
-<!--                <div class="card">-->
-<!--                    <div class="card-body">-->
-<!--                        <h4 class="card-title">Cheating students</h4>-->
-<!--                        <p class="card-text">Table with cheating students uinformation</p>-->
-<!--                    </div>-->
-<!--                    <div class="card-footer">-->
-<!--                        <a href="/students/cheat/" class="card-link">Show cheating students</a>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--            <div class="col-md-4 col-lg-4 col-sm-12">-->
-<!--                <div class="card">-->
-<!--                    <div class="card-body">-->
-<!--                        <h4 class="card-title">Students Projects</h4>-->
-<!--                        <p class="card-text">Table with students projects</p>-->
-<!--                    </div>-->
-<!--                    <div class="card-footer">-->
-<!--                        <a href="/students/projects/" class="card-link">Show students projects</a>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--            <div class="col-md-4 col-lg-4 col-sm-12">-->
-<!--                <div class="card">-->
-<!--                    <div class="card-body">-->
-<!--                        <h4 class="card-title">Rushes</h4>-->
-<!--                        <p class="card-text">All pet projects from Rushes</p>-->
-<!--                    </div>-->
-<!--                    <div class="card-footer">-->
-<!--                        <a href="/students/projects/rushes/" class="card-link">Show Rushes projects</a>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
-<!---->
-<!---->
-<!---->
-<!---->
-<!---->
-<!--        </div>-->
-<!--    </div>-->
 </div>
 
 
